1fa133fcfa372d2d149d216795ac0540333faa35
ultrafrog <ufrog.net@gmail.com>
2014-07-13 15:42:59 +0800

    添加格式化日期方法

--- src/main/java/net/ufrog/common/support/jetx/CommonMethods.java
+++ src/main/java/net/ufrog/common/support/jetx/CommonMethods.java
@@ -1,7 +1,9 @@
 package net.ufrog.common.support.jetx;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
+import net.ufrog.common.utils.Calendars;
 import net.ufrog.common.utils.Numbers;
 
 /**
@@ -31,5 +33,29 @@ public final class CommonMethods {
 			return Numbers.percent(Number.class.cast(value));
 		}
 		return Numbers.percent(new BigDecimal(value.toString()));
+	}
+	
+	/**
+	 * @param value
+	 * @return
+	 */
+	public static String date(Object value) {
+		return Calendars.date(Date.class.cast(value));
+	}
+	
+	/**
+	 * @param value
+	 * @return
+	 */
+	public static String time(Object value) {
+		return Calendars.time(Date.class.cast(value));
+	}
+	
+	/**
+	 * @param value
+	 * @return
+	 */
+	public static String datetime(Object value) {
+		return Calendars.datetime(Date.class.cast(value));
 	}
 }
